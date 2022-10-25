d77b646cdf25737b60f52ba8d5499a1b9af1e287
Yuberley <yuberleyguerrero1999@gmail.com>
2022-10-25 11:08:02 -0500

    insert files js

---
+++ assets/js/loadingButton.js
@@ -0,0 +1,19 @@
+
+const buttonAdd = document.getElementById("agregar_colaborador");
+const buttonClose = document.getElementById("close_modal");
+const buttonInner = document.getElementById("button_inner");
+
+buttonAdd.addEventListener("click", () => {
+    const spinner = `<button class="btn btn-primary" type="button" disabled>
+                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                        Loading...
+                    </button>`;
+    console.log("buttonAdd");
+    buttonInner.innerHTML = spinner;
+});
+
+buttonClose.addEventListener("click", () => {
+    const normal = `<input type="submit" name="agregar_colaborador" value="Agregar Colaborador" id="agregar_colaborador">`;
+    cosole.log("buttonClose");
+    buttonInner.innerHTML = normal;
+});
