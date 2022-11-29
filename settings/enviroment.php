a89c06e6ef7c22c29a8d579f3ecc1732ac591e0e
Roger <rogerricardo09@gmail.com>
2022-11-29 15:31:07 -0500

    Version incluyendo enviroment config

--- settings/enviroment.php
+++ settings/enviroment.php
@@ -2,39 +2,39 @@
 
 function getWoocommerceKey(){
     $key = [
-        "public" => "ck_8f0eab38e14f05e4a1df2a78d0dc00876042a326",
-        "private" => "cs_370e922e21733e78a06e0ca55a8e4fb5177c1b3f",
+        "public" => "your public key woocommerce",
+        "private" => "your private key woocommerce",
    
     ];
     return $key;
 }
 
 function getWoocommerceUrl(){
-    $url = "http://179.32.53.160/primedigital/";
+    $url = "your  wordpress /";
     return $url;
 }
 
 function getMoodleToken(){
-    $key = "ffdbb4a4f4102cf554cd040dd2ecde94";
+    $key = "your token moodle";
     return $key;
 }
 
 function getMoodleUrl(){
-    $url = "http://179.32.53.160/moodle/webservice/rest/server.php?wstoken=".getMoodleToken()."&moodlewsrestformat=json";
+    $url = "your url moodle /webservice/rest/server.php?wstoken=".getMoodleToken()."&moodlewsrestformat=json";
     return $url;
 }
 
 function getMoodleCategoryId(){
     $category = [
-        "basic" => 2,
-        "premium" => 3,
+        "basic" => "your category id basic",
+        "premium" => "your category id premium",
     ];
     return $category;
 }
 
 function getMoodle(){
     $moodle = new \GuzzleHttp\Client([
-        'base_uri' => 'http://179.32.53.160/moodle/',
+        'base_uri' => 'your url moodle /',
     ]);
     return $moodle;
 }
